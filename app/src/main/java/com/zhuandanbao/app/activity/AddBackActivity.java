013e17840383b25e146d078034a6adbc2b4b8e92
严杨 <475893401@qq.com>
2018-04-03 11:39:46 +0800

    ceshi

--- app/src/main/java/com/zhuandanbao/app/activity/AddBackActivity.java
+++ app/src/main/java/com/zhuandanbao/app/activity/AddBackActivity.java
@@ -74,8 +74,6 @@ public class AddBackActivity extends BaseHttpActivity<AddBackD> {
         button.setVisibility(View.VISIBLE);
         button.setOnClickListener(this);
 
-
-
         viewDelegate.setOnClickListener(this, R.id.back_forget_pass, R.id.back_name, R.id.back_area);
         if (skipInfoEntity.index == 1) {
             viewDelegate.setRight("刪除", 0, new View.OnClickListener() {
